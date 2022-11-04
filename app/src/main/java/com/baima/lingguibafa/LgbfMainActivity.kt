6dac7fda829bf3da8f542df38dc92bb399984fbe
Mountain <2031211480@qq.com>
2022-11-04 22:52:58 +0800

    创建工程

---
+++ app/src/main/java/com/baima/lingguibafa/LgbfMainActivity.kt
@@ -0,0 +1,12 @@
+package com.baima.lingguibafa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LgbfMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lgbf_main)
+    }
+}
